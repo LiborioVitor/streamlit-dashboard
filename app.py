3183814e0b036d0cd2672baae19c44edc7449207
LiborioVitor <vitor.liborio@datasales.io>
2025-04-23 12:20:18 -0300

    atualiza app e require

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import plotly.express as px
 from streamlit_autorefresh import st_autorefresh
 
 st.set_page_config(layout='wide')
-st_autorefresh(interval=300000, key="refresh")
+st_autorefresh(interval=30000, key="refresh")
 
 #engine = f.criar_conexao(connection= 'relatorios_azure',database= 'piperun_clean')
 #sql = '''select data_venda, mrr, vendedor from datasales.vendas_base where year(data_venda) = 2025 and equipe in ('Inbound','Outbound') '''
